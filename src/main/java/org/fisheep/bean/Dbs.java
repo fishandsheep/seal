2b04fcfaa514b130bb49554bcd6d1a32db92cbb7
BigOrange-1124 <g13104233370@163.com>
2024-07-25 09:52:27 +0800

    storage and loading

--- src/main/java/org/fisheep/bean/Dbs.java
+++ src/main/java/org/fisheep/bean/Dbs.java
@@ -1,12 +1,32 @@
 package org.fisheep.bean;
 
+import org.eclipse.serializer.persistence.types.PersistenceStoring;
+import org.fisheep.common.StorageManagerFactory;
+import org.fisheep.common.concurrent.ReadWriteLocked;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Dbs {
+public class Dbs extends ReadWriteLocked {
 
-    private Map<String, List<Db>> dbs = new HashMap<>();
+    private Map<String, Db> dbs = new HashMap<>();
 
+    public void add(String id, Db db) {
+        this.add(id, db, StorageManagerFactory.getInstance());
+    }
+
+    private void add(String id, Db db, PersistenceStoring persistenceStoring) {
+        this.write(() -> {
+            this.dbs.put(id, db);
+            persistenceStoring.store(dbs);
+        });
+    }
+
+    public Db all(String id) {
+        return this.read(() ->
+                this.dbs.get(id)
+        );
+    }
 
 }
--- src/main/java/org/fisheep/common/StorageManagerFactory.java
+++ src/main/java/org/fisheep/common/StorageManagerFactory.java
@@ -2,6 +2,7 @@ package org.fisheep.common;
 
 import org.eclipse.store.storage.embedded.types.EmbeddedStorage;
 import org.eclipse.store.storage.embedded.types.EmbeddedStorageManager;
+import org.fisheep.bean.Data;
 
 public class StorageManagerFactory {
 
@@ -11,8 +12,17 @@ public class StorageManagerFactory {
         if (storageManager == null) {
             storageManager = EmbeddedStorage.start();
         }
+        if (storageManager.root() == null) {
+            createStorage();
+        }
         return storageManager;
     }
+
+    public static void createStorage() {
+        storageManager.setRoot(new Data());
+        storageManager.storeRoot();
+    }
+
 
     public static void shutdown() {
         if (storageManager != null) {
