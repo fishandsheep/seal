69db8a0c78a56e2c8c0ffcd0a203175d7c45eb0c
Qinshower <zhangwenming0906@163.com>
2024-07-28 09:09:33 +0800

    [soar] tmp test soar

---
+++ src/main/java/org/fisheep/Main.java
@@ -0,0 +1,28 @@
+package org.fisheep;
+
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.PumpStreamHandler;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        String command = "/root/soar -query 'select * from dual'";
+        //接收正常结果流
+        ByteArrayOutputStream susStream = new ByteArrayOutputStream();
+        //接收异常结果流
+        ByteArrayOutputStream errStream = new ByteArrayOutputStream();
+        CommandLine commandLine = CommandLine.parse(command);
+        DefaultExecutor exec = new DefaultExecutor();
+        PumpStreamHandler streamHandler = new PumpStreamHandler(susStream, errStream);
+        exec.setStreamHandler(streamHandler);
+        int code = exec.execute(commandLine);
+        System.out.println("退出代码: " + code);
+        System.out.println(susStream.toString("UTF8"));
+        System.out.println(errStream.toString("UTF8"));
+
+    }
+}
