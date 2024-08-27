9a821f872bf72731b0df2eb217be9ad890f5a5b3
zhang <zhangwenming0906@163.com>
2024-08-28 00:29:53 +0800

    [back] go ?

--- src/main/java/org/fisheep/bean/data/SqlStatements.java
+++ src/main/java/org/fisheep/bean/data/SqlStatements.java
@@ -3,7 +3,6 @@ package org.fisheep.bean.data;
 import org.eclipse.serializer.collections.lazy.LazyArrayList;
 import org.eclipse.serializer.collections.lazy.LazyList;
 import org.eclipse.serializer.persistence.types.PersistenceStoring;
-import org.eclipse.serializer.reference.Lazy;
 import org.fisheep.bean.SqlStatement;
 import org.fisheep.common.StorageManagerFactory;
 import org.fisheep.common.concurrent.ReadWriteLocked;
@@ -16,7 +15,7 @@ import java.util.Map;
  * @author BigOrange
  */
 public class SqlStatements extends ReadWriteLocked {
-    private final Map<String, Lazy<LazyList<SqlStatement>>> sqlStatements = new HashMap<>();
+    private final Map<String, LazyList<SqlStatement>> sqlStatements = new HashMap<>();
 
     public void add(String id, List<SqlStatement> statementList) {
         this.add(id, statementList, StorageManagerFactory.getInstance());
@@ -24,27 +23,27 @@ public class SqlStatements extends ReadWriteLocked {
 
     public LazyList<SqlStatement> one(String id) {
         return this.read(() ->
-                Lazy.get(this.sqlStatements.get(id))
+                this.sqlStatements.get(id)
         );
     }
 
-    public void delete(String id){
+    public void delete(String id) {
         this.delete(id, StorageManagerFactory.getInstance());
     }
 
     private void add(String id, List<SqlStatement> statementList, PersistenceStoring persistenceStoring) {
         this.write(() -> {
-            LazyArrayList<SqlStatement> lazyArrayList = new LazyArrayList<>();
-            lazyArrayList.addAll(statementList);
-            this.sqlStatements.put(id, Lazy.Reference(lazyArrayList));
+            LazyList<SqlStatement> lazyList = new LazyArrayList<>();
+            lazyList.addAll(statementList);
+            this.sqlStatements.put(id, lazyList);
             persistenceStoring.store(sqlStatements);
         });
     }
 
     private void delete(String id, PersistenceStoring persistenceStoring) {
         this.write(() -> {
-           this.sqlStatements.remove(id);
-           persistenceStoring.store(this.sqlStatements);
+            this.sqlStatements.remove(id);
+            persistenceStoring.store(this.sqlStatements);
         });
     }
 }
