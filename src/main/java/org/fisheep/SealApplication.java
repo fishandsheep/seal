98a39977ec3118034f04e03b351b77d786d15fd1
Qinshower <zhangwenming0906@163.com>
2024-09-20 15:34:50 +0800

    code review

--- src/main/java/org/fisheep/SealApplication.java
+++ src/main/java/org/fisheep/SealApplication.java
@@ -27,28 +27,35 @@ public class SealApplication {
 
         var app = Javalin.create(config -> {
 
-            config.staticFiles.add("/public", Location.CLASSPATH);
-            config.http.defaultContentType = "text/plain; charset=utf-8";
-            config.router.contextPath = "/seal";
+                    config.staticFiles.add("/public", Location.CLASSPATH);
+                    config.http.defaultContentType = "text/plain; charset=utf-8";
+                    config.router.contextPath = "/seal";
 
-            config.requestLogger.http((ctx, ms) ->
-                    log.debug("{}接口耗时：{}ms", ctx.path(), ms)
-            );
+                    config.requestLogger.http((ctx, ms) ->
+                            log.debug("{}接口耗时：{}ms", ctx.path(), ms)
+                    );
 
-            config.router.apiBuilder(() -> path("/db", () -> {
-                get(DbManager::all);
-                post(DbManager::add);
-                path("/{id}", () -> delete(DbManager::delete));
-            })).apiBuilder(() -> path("/export", () -> post(ExplainManager::export))).apiBuilder(() -> path("/explain", () -> {
-                get(DbManager::dbAndTimestamp);
-                path("/result", () -> post(ExplainManager::pageOne));
-                path("/status", () -> post(ExplainManager::status));
-            }));
-        }).post("/upload", ctx -> {
-            NaiveRateLimit.requestPerTimeUnit(ctx, 10, TimeUnit.MINUTES);
-            NaiveRateLimit.requestPerTimeUnit(ctx, 1, TimeUnit.SECONDS);
-            ExplainManager.upload(ctx);
-        }).exception(SealException.class, (e, ctx) -> ctx.json(new Result(e.getCode(), e.getMsg()))).start(7070);
+                    config.router
+                            // 数据连接相关接口
+                            .apiBuilder(() -> path("/db", () -> {
+                                get(DbManager::all); //1. 查询全部连接
+                                post(DbManager::add); //2. 添加连接
+                                path("/{id}", () -> delete(DbManager::delete)); // 3. 根据下标删除连接
+                            }))
+                            // TODO 导出
+                            .apiBuilder(() -> path("/export", () -> post(ExplainManager::export)))
+                            // 风险扫描相关接口
+                            .apiBuilder(() -> path("/explain", () -> {
+                                get(DbManager::dbAndTimestamp); // 4. 获取最近风险扫描的时间戳记录
+                                path("/result", () -> post(ExplainManager::pageOne));// 5. 分页查询风险SQL
+                                path("/status", () -> post(ExplainManager::status));// 6. 获取上传sql二进制文件任务的状态
+                            }));
+                }).post("/upload", ctx -> { // 7. 上传sql二进制文件
+                    NaiveRateLimit.requestPerTimeUnit(ctx, 10, TimeUnit.MINUTES);
+                    NaiveRateLimit.requestPerTimeUnit(ctx, 1, TimeUnit.SECONDS);
+                    ExplainManager.upload(ctx);
+                })
+                .exception(SealException.class, (e, ctx) -> ctx.json(new Result(e.getCode(), e.getMsg()))).start(7070);
 
         app.sse("/explain/status/{explainId}", ExplainManager::sendStatus);
 
